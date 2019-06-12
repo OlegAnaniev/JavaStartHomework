92dc60e80de5bb7603d39640cad337a14b4e8f36
Ober <Ober@DESKTOP-UBBGVAL>
2019-06-13 02:14:10 +0300

    Homework lesson 3 level 1 task 2 (Appartment entrance and floor)

---
+++ Lesson03HomeworkLevel01Task02/src/training/Main.java
@@ -0,0 +1,48 @@
+package training;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		final int NUMBER_OF_FLOORS = 9;
+		final int NUMBER_OF_ENTRANCES = 4;
+		final int APPARTMENTS_PER_FLOOR = 4;
+		final String INPUT_MESSAGE = "Input appartment no.:";
+		final String INVALID_INPUT_MESSAGE = "Appartment does not exist";
+		final String OUTPUT_ENTRANCE_MESSAGE = "Entrance:";
+		final String OUTPUT_FLOOR_MESSAGE = "Floor:";		
+		int totalAppartments = NUMBER_OF_FLOORS * NUMBER_OF_ENTRANCES 
+				* APPARTMENTS_PER_FLOOR;
+		int appartmentsPerEntrance = NUMBER_OF_FLOORS * APPARTMENTS_PER_FLOOR;
+		Scanner scanner = new Scanner(System.in);
+		int userInput;
+		int entrance;
+		int appPositionInsideEntrance;
+		int floor;		
+		
+		System.out.print(INPUT_MESSAGE + " ");
+		userInput = scanner.nextInt();
+		
+		if (userInput <= 0 || userInput > totalAppartments) {
+			System.out.println(INVALID_INPUT_MESSAGE);
+			return;
+		}
+		
+		entrance = userInput / appartmentsPerEntrance;
+		if (userInput % appartmentsPerEntrance != 0) {
+			entrance++;
+		}
+		
+		appPositionInsideEntrance = userInput - 
+				(appartmentsPerEntrance * (entrance - 1));		
+		floor = appPositionInsideEntrance / APPARTMENTS_PER_FLOOR;
+		if (appPositionInsideEntrance % APPARTMENTS_PER_FLOOR != 0) {
+			floor++;
+		}
+		
+		System.out.println(OUTPUT_ENTRANCE_MESSAGE + " " + entrance);
+		System.out.println(OUTPUT_FLOOR_MESSAGE + " " + floor);
+	}
+}
