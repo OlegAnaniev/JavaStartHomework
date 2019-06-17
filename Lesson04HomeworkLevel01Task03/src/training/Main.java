3fbc59cd7128d168617384efb90d39e9c808e637
Ober <Ober@DESKTOP-UBBGVAL>
2019-06-17 18:59:15 +0300

    Homework lesson 4 level 1 task 3 (Multiplication table)

---
+++ Lesson04HomeworkLevel01Task03/src/training/Main.java
@@ -0,0 +1,14 @@
+package training;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		final int BASE = 5;
+		final int COUNT = 10;
+		
+		for (int i = 1; i <= COUNT; i++) {
+			System.out.println(BASE + " x " + i + " = " + (BASE * i));
+		}
+	}
+}
