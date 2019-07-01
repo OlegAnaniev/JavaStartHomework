6c2ec09a671bd41cdd3460d788cb93f5e40344af
OlegAnaniev <test@test.com>
2019-07-01 21:13:23 +0300

    Homework lesson 8 level 1 task 3 (Display subfolders)

---
+++ Lesson08HomeworkLevel01Task03/src/training/Main.java
@@ -0,0 +1,41 @@
+package training;
+
+import java.io.File;
+
+public class Main {	
+
+	private static final String SEPATAROR_INDENT = "\t";
+
+	public static void main(String[] args) {
+
+		final String OUTPUT_MESSAGE = "List of %s subfolders:";
+		final String PATH = ".";		
+		final String SEPARATOR = "--->";
+		File currentFolder = new File(PATH);
+
+		System.out.println(String.format(OUTPUT_MESSAGE, 
+				currentFolder.getAbsolutePath()));
+		displayFolderTree(currentFolder, SEPARATOR);
+
+	}
+
+	/**
+	 * Recursively displays subfolders of a given folder 
+	 * 
+	 * @param currentFolder <code>File</code>
+	 * @param separator <code>String</code> displays nesting level
+	 * @author Oleg
+	 */
+	private static void displayFolderTree (File currentFolder, 
+			String separator) {
+
+		File[] currentFolderContent = currentFolder.listFiles();
+
+		for (File item : currentFolderContent) {
+			if (item.isDirectory()) {
+				System.out.println(separator + item.getName());
+				displayFolderTree(item, SEPATAROR_INDENT + separator);
+			}
+		}		
+	}
+} 
