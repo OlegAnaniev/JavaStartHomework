82ba984ec1d31de5812b1d93cc7d831c6e87fcab
OlegAnaniev <test@test.com>
2019-07-02 14:05:20 +0300

    Homework lesson 7 level 1 task 2 (Arrays.toString())
    Handled case when array.length == 0

--- Lesson07HomeworkLevel01Task02/src/training/Main.java
+++ Lesson07HomeworkLevel01Task02/src/training/Main.java
@@ -7,9 +7,12 @@ public class Main {
 	public static void main(String[] args) {
 
 		int[] array = new int[] { -5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5 };
+		int[] arrayEmpty = new int[0];
 		
 		System.out.println(Arrays.toString(array));
 		System.out.println(arrayToString(array));
+		System.out.println(Arrays.toString(arrayEmpty));
+		System.out.println(arrayToString(arrayEmpty));
 	}
 	
 	/**
@@ -26,11 +29,15 @@ public class Main {
 		final String CLOSING_BRACKETS = "]";
 		StringBuilder text = new StringBuilder(OPENING_BRACKETS);
 		
-		for (int i = 0; i < array.length - 1; i++) {
-			text.append(array[i] + SEPARATOR);
-		}
+		if (array.length > 0) {
+			for (int i = 0; i < array.length - 1; i++) {
+				text.append(array[i] + SEPARATOR);
+			}
+			
+			text.append(array[array.length - 1]);
+		}		
 		
-		text.append(array[array.length - 1] + CLOSING_BRACKETS);
+		text.append(CLOSING_BRACKETS);
 		
 		return text.toString();
 	}
